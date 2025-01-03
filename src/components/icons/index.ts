337a472a92c9efcfe361cd263fae3a42708d056f
vijethx <38877972+vijethx@users.noreply.github.com>
2025-01-03 21:59:12 +0530

    added data.tsx and social icons (in svg)

---
+++ src/components/icons/index.ts
@@ -0,0 +1,6 @@
+import { PeerlistIcon } from "./PeerlistIcon";
+import { GitHubIcon } from "./GithubIcon";
+import { LinkedInIcon } from "./LinkedinIcon";
+import { XIcon } from "./XIcon";
+
+export { PeerlistIcon, GitHubIcon, LinkedInIcon, XIcon };
